06c191b9226618a88c73237b6e07c2c49c538085
Zeshan Ahmad <zahmad5870@gmail.com>
2026-02-18 21:11:59 -0500

    fix the auth key and make it less sexy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
-import InitialScreen from "./Screens/InitialScreen.jsx";
-import ImageUploadScreen from "./Screens/ImageUploadScreen.jsx";
-import NoteUploadScreen from "./Screens/NoteUploadScreen.jsx";
-import TaskTrackerScreen from "./Screens/TaskTrackerScreen.jsx";
+import React, { useState, useEffect } from 'react';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import InitialScreen from './Screens/InitialScreen.jsx';
+import ImageUploadScreen from './Screens/ImageUploadScreen.jsx';
+import NoteUploadScreen from './Screens/NoteUploadScreen.jsx';
+import TaskTrackerScreen from './Screens/TaskTrackerScreen.jsx';
 export default function App() {
   return (
     <HashRouter>
       <div>
-        <h2>DegenerateUploader</h2>
+        <h2>Captains Log</h2>
       </div>
       <Routes>
         <Route path="/" element={<InitialScreen />} />
--- src/Screens/InitialScreen.jsx
+++ src/Screens/InitialScreen.jsx
@@ -1,27 +1,26 @@
-import React from "react";
-import NoteUploader from "../Components/NoteUploader.jsx";
-import SexyImage from "../Components/SexyImage.jsx";
-import { useNavigate } from "react-router-dom";
-import AutoCorrectTextInput from "../Components/AutoCorrectTextInput.jsx";
+import React from 'react';
+import NoteUploader from '../Components/NoteUploader.jsx';
+
+import { useNavigate } from 'react-router-dom';
+import AutoCorrectTextInput from '../Components/AutoCorrectTextInput.jsx';
 
 const InitialScreen = () => {
   const navigate = useNavigate();
 
   return (
     <div style={{ padding: 16 }}>
-      <button onClick={() => navigate("/noteuploadscreen")}>
+      <button onClick={() => navigate('/noteuploadscreen')}>
         Upload Notes
       </button>
-      <button onClick={() => navigate("/imageuploadscreen")}>
+      <button onClick={() => navigate('/imageuploadscreen')}>
         Upload Image
       </button>
-      <button onClick={() => navigate("/tasktrackerscreen")}>
+      <button onClick={() => navigate('/tasktrackerscreen')}>
         Task Tracker
       </button>
-      <AutoCorrectTextInput documentElementId={"txt1"} />
-      <AutoCorrectTextInput documentElementId={"txt2"} inputType={"input"} />
+      <AutoCorrectTextInput documentElementId={'txt1'} />
+      <AutoCorrectTextInput documentElementId={'txt2'} inputType={'input'} />
       <NoteUploader />
-      {/* <SexyImage /> */}
     </div>
   );
 };
